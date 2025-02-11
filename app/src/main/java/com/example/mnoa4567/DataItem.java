443bae35c16caf30670ec5fbc48853513db18e83
talmid17 <noasimkin23@gmail.com>
2025-02-11 14:50:15 +0200

    הוספתי עיצוב אייטם, הוספתי מחלקה

--- app/src/main/java/com/example/mnoa4567/DataItem.java
+++ app/src/main/java/com/example/mnoa4567/DataItem.java
@@ -1,4 +1,13 @@
 package com.example.mnoa4567;
 
 public class DataItem {
+    private String s1 ;
+
+    public void setS1(String s1) {
+        this.s1 = s1;
+    }
+
+    public String getS1() {
+        return s1;
+    }
 }
---
+++ app/src/main/java/com/example/mnoa4567/MyAdapter.java
@@ -0,0 +1,8 @@
+package com.example.mnoa4567;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>
+{
+    private da
+}
